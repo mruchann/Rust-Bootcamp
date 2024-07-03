c0a608fcb9c3cc39cc76fba2403b7a75bfd32922
ruchan <ruchan.yavuzdemir@icloud.com>
2024-07-03 21:55:06 +0300

    Create Cargo files and main.rs

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+fn main() {
+    let s1 = "Free Palestine!";
+    let mut s2 = "Free Ukraine!";
+    s2 = ".";
+
+    println!("{}", s1);
+
+    let l = 32;
+    let r : i64 = 0;
+
+    // arch -> usize, isize
+    let i : usize = 0;
+    let var : isize = -23;
+
+    let f = 3.5; // f64
+
+    println!("{}", i);
+}
